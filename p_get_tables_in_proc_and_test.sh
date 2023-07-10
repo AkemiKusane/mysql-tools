09806fb88d2ad01764303174b8e831d85d63c58f
misakamikoto110@gmail.com <misakamikoto110@gmail.com>
2023-07-10 13:52:49 +0900

    update code

--- p_get_tables_in_proc_and_test.sh
+++ p_get_tables_in_proc_and_test.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
+# Check if the correct number of arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <database_name> <procedure_name>"
+    exit 1
+fi
+
+# MySQL connection parameters
+cnf_file="/path/to/your/cnf/file"
 database=$1
-stored_procedure=$2
-cnf_file=$3
+procedure=$2
 
-procedure_body=$(mysql --defaults-extra-file=$cnf_file -D$database -e "SHOW CREATE PROCEDURE $stored_procedure\G" | grep -oiP 'CREATE DEFINER.*BEGIN\s*\K.*(?=END)')
-
-statements=$(echo "$procedure_body" | grep -oiP '(SELECT[^;]*FROM `[^;]*`)|(UPDATE `[^;]*`)|(INSERT INTO `[^;]*`)')
-
-while IFS= read -r statement; do
-    if [[ $statement =~ ^INSERT ]]; then
-        # This is an INSERT statement, extract the table name and store it
-        table_name=$(echo "$statement" | grep -oiP 'INSERT INTO \K[a-zA-Z0-9._]+')
-        echo "Found INSERT statement on table: $table_name"
-    elif [[ $statement =~ ^SELECT || $statement =~ ^UPDATE ]]; then
-        # This is a SELECT or UPDATE statement, process it
-        process_select_or_update "$statement"
-    fi
-done <<< "$statements"
-
+# Function to extract and process the table name and JOIN statement from a SELECT or UPDATE statement
 process_select_or_update() {
     local statement=$1
 
@@ -30,12 +23,38 @@ process_select_or_update() {
         # Extract the JOIN clause
         local join_clause=$(echo "$statement" | grep -oiP 'JOIN [a-zA-Z0-9._]+[^WHERE|ORDER|SET]*')
         # Add the JOIN clause to the SELECT statement
-        select_statement="SELECT SQL_NO_CACHE * FROM $table_name $join_clause LIMIT 1000;"
+        select_statement="SELECT * FROM $table_name $join_clause LIMIT 0"
     else
-        select_statement="SELECT SQL_NO_CACHE * FROM $table_name LIMIT 1000;"
+        select_statement="SELECT * FROM $table_name LIMIT 0"
     fi
 
     # Execute the SELECT statement and measure the execution time using MySQL's profiling
     echo "Profiling $select_statement..."
-    mysql --defaults-extra-file=$cnf_file -D$database -e "SET profiling = 1; pager cat > /dev/null; $select_statement; nopager; SHOW PROFILES; SET profiling = 0;"
+    mysql --defaults-extra-file=$cnf_file -D$database -e "SET profiling = 1; $select_statement; SHOW PROFILES; SET profiling = 0;"
 }
+
+# Function to extract and save the table name from an INSERT statement
+process_insert() {
+    local statement=$1
+
+    # Extract the table name
+    local table_name=$(echo "$statement" | grep -oiP 'INTO \K[a-zA-Z0-9._]+')
+    echo "Found INSERT statement for table $table_name"
+}
+
+# Connect to the MySQL server and get the procedure's body
+procedure_body=$(mysql --defaults-extra-file=$cnf_file -D$database -Bse "SELECT ROUTINE_DEFINITION FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_SCHEMA='$database' AND ROUTINE_NAME='$procedure'")
+
+# Find all SELECT, UPDATE and INSERT statements
+sql_statements=$(echo "$procedure_body" | grep -oiP '(SELECT[^;]*FROM [a-zA-Z0-9._]+[^;]*|UPDATE [a-zA-Z0-9._]+ SET [^;]*|INSERT INTO [a-zA-Z0-9._]+ VALUES [^;]*)')
+
+# Process each SQL statement
+for sql_statement in $sql_statements; do
+    if [[ $sql_statement =~ ^SELECT ]]; then
+        process_select_or_update "$sql_statement"
+    elif [[ $sql_statement =~ ^UPDATE ]]; then
+        process_select_or_update "$sql_statement"
+    elif [[ $sql_statement =~ ^INSERT ]]; then
+        process_insert "$sql_statement"
+    fi
+done
